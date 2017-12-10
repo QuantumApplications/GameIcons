e6de24319744e36ae09f25bbd0fa0369aedd51c1
Christian Oberdörfer <christian.michael.oberdoerfer@gmail.com>
2017-12-10 23:39:04 +0100

    Added Xcode project

---
+++ GameIcons/GameIcons/GameIcons.h
@@ -0,0 +1,19 @@
+//
+//  GameIcons.h
+//  GameIcons
+//
+//  Created by Christian Oberdörfer on 10.12.17.
+//  Copyright © 2017 Quantum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GameIcons.
+FOUNDATION_EXPORT double GameIconsVersionNumber;
+
+//! Project version string for GameIcons.
+FOUNDATION_EXPORT const unsigned char GameIconsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameIcons/PublicHeader.h>
+
+
