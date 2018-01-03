ddb25e823ad7af0953a99c4ab30a87c95f980169
Christian Oberdörfer <oberdoerfer@quantum.qa>
2018-01-03 14:25:46 +0100

    Updated GameIconsTests

--- GameIcons/GameIconsTests/GameIconTests.swift
+++ GameIcons/GameIconsTests/GameIconTests.swift
@@ -10,7 +10,22 @@ import XCTest
 @testable import GameIcons
 
 class GameIconsTests: XCTestCase {
-    
+
+    func testImageSizeCGRect() {
+        // 3. Assert
+        XCTAssertEqual(GameIcon.fission.image(size: CGRect(x: 0, y: 0, width: 10, height: 10)).size, CGSize(width: 10, height: 10))
+    }
+
+    func testImageSizeCGSize() {
+        // 3. Assert
+        XCTAssertEqual(GameIcon.fission.image(size: CGSize(width: 10, height: 10)).size, CGSize(width: 10, height: 10))
+    }
+
+    func testImageSizeInt() {
+        // 3. Assert
+        XCTAssertEqual(GameIcon.fission.image(size: 10).size, CGSize(width: 10, height: 10))
+    }
+
     func testTabBarImage() {
         // 3. Assert
         XCTAssertEqual(GameIcon.fission.tabBarImage.size, CGSize(width: 32, height: 32))
